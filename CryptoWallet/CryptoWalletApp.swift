a7d15e433ae8902953e9cb2856ac45be5bc64c99
Dmytro Krupskyi <krupskydima@gmail.com>
2022-07-17 12:24:57 +0300

    Initial commit

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Dmytro Krupskyi on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
